ea3ae32b3b29fca9fbcb6924683843b7836fee0f
JVCarv41 <jvc.04.off@gmail.com>
2023-11-25 10:39:02 -0300

    Adicionados comentarios; melhora no sistema de Sort

--- src/jogador/Jogador.java
+++ src/jogador/Jogador.java
@@ -7,28 +7,37 @@ import personagem.*;
 public class Jogador {
 	private String nomeJogador;
 	private ArrayList<Personagem> personagens = new ArrayList<Personagem>();
+	//Um jogador pode possuir múltiplos personagens
 	
 	public Jogador() {
+		//Inicializador padrão
 	}
 	public Jogador(String nomeJogador) {
+		//Inicializador com nome
 		this.nomeJogador=nomeJogador;
 	}
 	
 	public void addPersonagem(Personagem personagem) {
+		//Adiciona um novo personagem ao ArrayList
 		personagens.add(personagem);
 	}
 	public boolean remPersonagem(Personagem personagem) {
+		//Se possível, retira um personagem do ArrayList,
+		//e retorna se a operação foi possível de ser realizada
 		return personagens.remove(personagem);
 	}
 	public String getNomeJogador() {
+		//Consegue o nome do jogador
 		return nomeJogador;
 	}
 	public void setNomeJogador(String nomeJogador) {
+		//Define o nome do jogador
 		this.nomeJogador = nomeJogador;
 	}
 	
 	@Override
 	public String toString() {
+		//toString do jogador, utiliza o toString dos personagens
 		String ts=new String();
 		ts="Nome: "+nomeJogador+"\n";
 		for(Personagem p:personagens) {
@@ -36,32 +45,37 @@ public class Jogador {
 		}
 		return ts;
 	}
-	public boolean sortBy(SortBy sb) {
+	public void sortBy(SortBy sb) {
+		//Recebe como parâmetro qual o atributo que se deseja utilizar para ordenar o ArrayList
 		switch (sb) {
 		case nome:
+			//Ordena por nome
 			Collections.sort(personagens,Sort.pNome);
 			break;
 		case idade:
+			//Ordena por idade
 			Collections.sort(personagens,Sort.pIdade);
 			break;
 		case forca:
+			//Ordena por força
 			Collections.sort(personagens,Sort.pForca);
 			break;
 		case inteligencia:
+			//Ordena por inteligência
 			Collections.sort(personagens,Sort.pInteligencia);
 			break;
 		case carisma:
+			//Ordena por carisma
 			Collections.sort(personagens,Sort.pCarisma);
 			break;
 		case constituicao:
+			//Ordena por constituição
 			Collections.sort(personagens,Sort.pconstituicao);
 			break;
 		case sabedoria:
+			//Ordena por sabedoria
 			Collections.sort(personagens,Sort.pSabedoria);
 			break;
-		default:
-			return false;
 		}
-		return true;
 	}
 }
--- src/jogador/Sort.java
+++ src/jogador/Sort.java
@@ -5,79 +5,125 @@ import java.util.Comparator;
 import personagem.Personagem;
 
 public class Sort {
-	static Comparator<Personagem>pNome=(Comparator<Personagem>)new Comparator<Personagem>(){
-		public int compare (Personagem a, Personagem b) {
-			if(a.getNome().charAt(0)>b.getNome().charAt(0)) {
+	
+	//Função que retorna a ordem por nome (reutilizável)
+	static int name(Personagem a, Personagem b) {
+		if(a.getNome().charAt(0)>b.getNome().charAt(0)) {
+			//Primeira letra da palavra A vem depois da primeira letra da palavra B
+			return 1;
+		}else if(a.getNome().charAt(0)==b.getNome().charAt(0)) {
+			//Palavras começam com a mesma letra
+			if(a.getNome().charAt(1)>b.getNome().charAt(1)) {
+				//Segunda letra da palavra A vem depois da segunda letra da palavra B
 				return 1;
-			}else if(a.getNome().charAt(0)==b.getNome().charAt(0)) {
-				return 0;
 			}else {
+				//Segunda letra da palavra A vem antes da segunda letra da palavra B
 				return -1;
 			}
+		}else {
+			//Primeira letra da palavra A vem antes da primeira letra da palavra B
+			return -1;
+		}
+	}
+	
+	//Função que ordena, propriamente dito, por Nome
+	static Comparator<Personagem>pNome=(Comparator<Personagem>)new Comparator<Personagem>(){
+		public int compare (Personagem a, Personagem b) {
+			return name(a,b);
 		}
 	};
+	
+	//Por Idade
 	static Comparator<Personagem>pIdade=(Comparator<Personagem>)new Comparator<Personagem>(){
 		public int compare (Personagem a, Personagem b) {
 			if(a.getIdade()>b.getIdade()) {
+				//Idade A maior que B
 				return 1;
 			}else if(a.getIdade()==b.getIdade()) {
-				return 0;
+				//Idades Iguais, ordena por nome
+				return name(a,b);
 			}else {
+				//Idade B maior que A
 				return -1;
 			}
 		}
 	};
+	
+	//Por Força
 	static Comparator<Personagem>pForca=(Comparator<Personagem>)new Comparator<Personagem>(){
 		public int compare (Personagem a, Personagem b) {
 			if(a.getForca()>b.getForca()) {
+				//Força A maior que B
 				return 1;
 			}else if(a.getForca()==b.getForca()) {
-				return 0;
+				//Forças Iguais, ordena por nome
+				return name(a,b);
 			}else {
+				//Força B maior que A
 				return -1;
 			}
 		}
 	};
+	
+	//Por Inteligência
 	static Comparator<Personagem>pInteligencia=(Comparator<Personagem>)new Comparator<Personagem>(){
 		public int compare (Personagem a, Personagem b) {
 			if(a.getInteligencia()>b.getInteligencia()) {
+				//Inteligência A maior que B
 				return 1;
 			}else if(a.getInteligencia()==b.getInteligencia()) {
-				return 0;
+				//Inteligencias iguais, ordena por nome
+				return name(a,b);
 			}else {
+				//Inteligência A menor que B
 				return -1;
 			}
 		}
 	};
+	
+	//Por Carisma
 	static Comparator<Personagem>pCarisma=(Comparator<Personagem>)new Comparator<Personagem>(){
 		public int compare (Personagem a, Personagem b) {
 			if(a.getCarisma()>b.getCarisma()) {
+				//Carisma A maior que B
 				return 1;
 			}else if(a.getCarisma()==b.getCarisma()) {
-				return 0;
+				//Carismas Iguais, ordena por nome
+				return name(a,b);
 			}else {
+				//Carisma B maior que A
 				return -1;
 			}
 		}
 	};
+	
+	//Por Constituição
 	static Comparator<Personagem>pconstituicao=(Comparator<Personagem>)new Comparator<Personagem>(){
 		public int compare (Personagem a, Personagem b) {
 			if(a.getConstituicao()>b.getConstituicao()) {
+				//Constituição A maior que B
 				return 1;
 			}else if(a.getConstituicao()==b.getConstituicao()) {
-				return 0;
+				//Constituições iguais, ordena por nome
+				return name(a,b);
 			}else {
+				//Constituição B maior que A
 				return -1;
 			}
 		}
 	};
+	
+	//Por Sabedoria
 	static Comparator<Personagem>pSabedoria=(Comparator<Personagem>)new Comparator<Personagem>(){
 		public int compare (Personagem a, Personagem b) {
 			if(a.getSabedoria()>b.getSabedoria()) {
+				//Sabedoria A maior que B
 				return 1;
 			}else if(a.getSabedoria()==b.getSabedoria()) {
-				return 0;
+				//Sabedorias iguais, ordena por nome
+				return name(a,b);
 			}else {
+				//Sabedoria B maior que A  
 				return -1;
 			}
 		}
