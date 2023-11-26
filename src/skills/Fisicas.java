a2f6b3d63cf6646c988d92d6779c266c99d61f0f
Druwing <143631126+Druwing@users.noreply.github.com>
2023-11-26 12:10:22 -0300

    Add files via upload

---
+++ src/skills/Fisicas.java
@@ -0,0 +1,13 @@
+package skills;
+
+public class Fisicas extends Skills {
+
+	public Fisicas(Integer level, String nome, String descricao) {
+		super(level, nome, descricao);
+	}
+	@Override
+public String toString() {
+	return super.toString();
+}
+
+}
